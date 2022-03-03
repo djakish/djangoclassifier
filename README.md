4c86b34642c8d260d6b1ae85821fb2cbfc46b9d5
djakish <69299717+djakish@users.noreply.github.com>
2022-03-03 14:16:12 +0600

    updates

---
+++ README.md
@@ -0,0 +1,27 @@
+# FINAL
+
+
+# _Installation_
+```bash
+pip install requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+```
+#  _Usage_
+```bash
+python manage.py runserver
+```
+
+# _Examples_
+Main page
+![alt text](assets/main_page.png)
+
+# Prediction examples
+
+![alt text](assets/predicted_example.png)
+![alt text](assets/predicted_example_2.png)
+
+# Database 
+![alt text](assets/database.png)
+
+
--- myApp/views.py
+++ myApp/views.py
@@ -15,11 +15,11 @@ def image_upload_view(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
             # Get the current instance object to display in the template
             img_obj = form.instance
             img_obj.save()
             img_obj.result = predict(img_obj.image.path)
+            form.save()
             return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
     else:
         form = ImageForm()
